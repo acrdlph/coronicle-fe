dea71ba161323dd6011dc14dd15de2fd52f3a391
Nikita Deshmukh <dnikitadeshmukh@gmail.com>
2020-03-21 15:56:10 +0100

    save coordinates in database

---
+++ persistence/db_save_locations.js
@@ -0,0 +1,20 @@
+import * as SQLite from "expo-sqlite";
+
+const db = SQLite.openDatabase("locations.db");
+
+export const save_coordinates = (lat, lng, timestamp) => {
+  const promise = new Promise(() => {
+    (resolve, reject) => {
+      db.transaction(tx => {
+        tx.executeSql(
+          "INSERT INTO (lat, lng, timestamp) VALUES (?, ?, ?)",
+          [lat, lng, timestamp],
+          () => { resolve(); },
+          (_, error) = {
+            reject(error);
+          }
+        );
+      });
+    }
+  });
+}
